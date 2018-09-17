f6ed6ef78b86bb7d5accbb0a19023c4872419629
betoalucard <betoprogramer@gmail.com>
2018-09-16 22:58:55 -0300

    teste_alberto (#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Brainstorming_teste';
+  title = 'Brainstorming_teste2';
 }
