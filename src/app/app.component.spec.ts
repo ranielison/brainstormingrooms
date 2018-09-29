39a7cdccaf6065ef354cdef3da806550b3f8f601
Alberto <betoprogramer@gmail.com>
2018-09-28 21:41:05 -0300

    gambiarra daatualização

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Brainstorming'`, async(() => {
+  it(`should have as title 'brainstorming'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Brainstorming');
+    expect(app.title).toEqual('brainstorming');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Brainstorming!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to brainstorming!');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+
+import { environment } from '../environments/environment';
+export const firebaseConfig = environment.firebaseConfig;
+
+import {ROUTES} from './app.routes';
+import { CoreModule } from './core/core.module';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { CadastrousuarioComponent } from './usuario/cadastrousuario/cadastrousuario.component';
+import { LoginComponent } from './usuario/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent,
+    UserProfileComponent,
+    UsuarioComponent,
+    CadastrousuarioComponent,
+    LoginComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    RouterModule.forRoot(ROUTES),
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
